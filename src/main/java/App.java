f4861bca2600542f4a7a055308406ceaa5312e61
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-07-17 22:12:26 -0300

    one of my many attempts to develop in java

---
+++ src/main/java/App.java
@@ -0,0 +1,28 @@
+package aplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+
+public class App extends Application {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+        launch();
+    }
+
+    @Override public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
